2ee20d2be79069edb3fd135019bd6c0fc588c0cc
koval <kovalskiy0303@mail.ru>
2024-10-04 11:42:07 +0300

    added related obj user to GET(:postId) response

--- src/application/services/post/post.service.ts
+++ src/application/services/post/post.service.ts
@@ -138,7 +138,7 @@ export class PostService {
     async findeOneById(id: string): Promise<PostReadDto | null> {
         try{
 
-            const post = await this.postRepository.findOne({where: {id: id }, relations: ['policy']});
+            const post = await this.postRepository.findOne({where: {id: id }, relations: ['policy', 'user']});
 
             if (!post) throw new NotFoundException(`Пост с ID: ${id} не найден`);
             const postReadDto: PostReadDto = {
