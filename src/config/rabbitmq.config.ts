7db69131ea482c5fb928df6e18c9eb6dde72749e
koval <kovalskiy0303@mail.ru>
2024-10-08 14:36:52 +0300

    updated rabbitmq configuration

--- src/config/rabbitmq.config.ts
+++ src/config/rabbitmq.config.ts
@@ -1,9 +1,16 @@
 import { RmqOptions, Transport } from '@nestjs/microservices';
+let url: string;
+if (process.env.NODE_ENV === 'prod'){
+  url = `amqp://${process.env.RABBITMQ_HOST_PROD}`
+}
+else{
+  url = `amqp://${process.env.RABBITMQ_HOST_LOCAL}`
+}
 
 export const rabbitmqConfig = (): RmqOptions => ({
   transport: Transport.RMQ,
   options: {
-    urls: ['amqp://localhost:5672'], // URL RabbitMQ-сервера
+    urls: [url], // URL RabbitMQ-сервера
     queue: 'accounts_queue', // Название очереди
     queueOptions: {
       durable: false,
