e93e64120baed9d0c4f0e52af6f3cfb31dddc09d
valerchikUwU <kovalskiy0303@mail.ru>
2025-03-25 14:08:02 +0300

    another fix

--- src/guards/message.guard.ts
+++ src/guards/message.guard.ts
@@ -20,7 +20,7 @@ export class MessagesGuard implements CanActivate {
         const ifPostSender = userPostsIds.includes(convert.pathOfPosts[0]);
         const ifPostReciever = userPostsIds.includes(convert.pathOfPosts[convert.pathOfPosts.length - 1]);
         const ifPostActive = userPostsIds.includes(convert.activePostId)
-        if ((ifPostSender || ifPostReciever || ifPostActive) && !isConvertFinished) {
+        if ((ifPostSender || ifPostReciever || ifPostActive) && isConvertFinished) {
             return true;
         }
 
