1dff9347a74458eb971582339484224279875ac5
valerchikUwU <99470988+valerchikUwU@users.noreply.github.com>
2025-04-08 17:49:51 +0300

    same quotes

--- src/application/services/convert/convert.service.ts
+++ src/application/services/convert/convert.service.ts
@@ -133,7 +133,7 @@ export class ConvertService {
                 )`
               )
                 .andWhere('"convert"."pathOfPosts"[1] = :contactId', { contactId })
-                .andWhere('"convert.convertPath != :directPath', { directPath: PathConvert.DIRECT })
+                .andWhere('"convert"."convertPath" != :directPath', { directPath: PathConvert.DIRECT })
                 .andWhere('"convert"."activePostId" IN (:...userPostsIds)', { userPostsIds })
             }))
         }))
